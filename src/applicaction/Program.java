0fee8b72a8b5d9adca4df8d135a5ea7c26157f84
FabioSinobre <engenheiro.computacao4.0@gmail.com>
2021-02-27 20:34:12 -0300

    up

--- src/applicaction/Program.java
+++ src/applicaction/Program.java
@@ -1,10 +1,10 @@
-package program;
+package applicaction;
 /**
  * 
  * @author Fabio D. Sinobre.
  *
  */
-public class Application {
+public class Program {
 
 	public static void main(String[] args) {
 		
